5cd0e47d7165924deff9ad9cccc30cb88f879918
a-slice <alicemellanby2@gmail.com>
2021-11-27 20:25:38 +1300

    add trik solution

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace csharpProblems
+{
+    class Trik
+    {
+        static void Main(string[] args)
+        {
+            String moves = Console.ReadLine();
+            int location = 1;
+            foreach(char move in moves)
+                {switch (location)
+                {
+                    case 1:
+                        switch (move)
+                        {
+                            case 'A':
+                                location++;
+                                break;
+                            case 'B':
+                                location += 2;
+                                break;
+                        }
+                        break;
+                    case 2:
+                        switch (move)
+                        {
+                            case 'A':
+                                location--;
+                                break;
+                            case 'B':
+                                location++;
+                                break;
+                        }
+                        break;
+                    case 3:
+                        switch (move)
+                        {
+                            case 'B':
+                                location--;
+                                break;
+                            case 'C':
+                                location -= 2;
+                                break;
+                        }
+                        break;
+                }
+
+                }
+            Console.WriteLine(location);
+        }
+    }
+}

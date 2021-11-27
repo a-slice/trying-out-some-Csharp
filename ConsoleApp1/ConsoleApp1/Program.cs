45a17a51ee1c68c0af773c1fd4b8b8820f1bbeff
a-slice <alicemellanby2@gmail.com>
2021-11-27 19:41:20 +1300

    some programs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharpProblems
+{
+    class OddEcho
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine() != null) {
+                String word = Console.ReadLine();
+                int count = 0;
+                if (count % 2 == 0)
+                    {
+                        Console.WriteLine(word);
+                    }
+                count++;
+            }
+
+        }
+    }
+}

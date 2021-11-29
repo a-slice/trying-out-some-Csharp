e0773b4d21e3cab191547f357f212ff6f0e83b5e
a-slice <alicemellanby2@gmail.com>
2021-11-29 16:52:10 +1300

    add triangle area

---
+++ ConsoleApp1/Traingle Area/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Traingle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            float theight = float.Parse(input[0]);
+            float tbase = float.Parse(input[1]);
+            float area = (theight * tbase) / 2;
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ Tarifa/Tarifa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int mb = x;
+            int count = 0;
+            while (count < n)
+            {
+                int used = int.Parse(Console.ReadLine());
+                mb -= used;
+                count += 1;
+                mb += x;
+            }
+            Console.WriteLine(mb);
+        }
+    }
+}
